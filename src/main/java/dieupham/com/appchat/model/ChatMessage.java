99f5f1c0e5ec649a0a1719c910b9ab0095941f97
dieuphamit <66241697+dieuphamit@users.noreply.github.com>
2020-06-26 10:43:19 +0900

    firstcommit

---
+++ src/main/java/dieupham/com/appchat/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package dieupham.com.appchat.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+ 
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+}
